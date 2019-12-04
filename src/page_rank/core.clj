8032d340799f3261b5eee804574daf69a980bf63
Blake Higgins <blake.higgins36@gmail.com>
2019-12-04 17:47:16 -0600

    Found all inbound pages

--- src/page_rank/core.clj
+++ src/page_rank/core.clj
@@ -1,16 +1,21 @@
 (ns page-rank.core
   (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str])
 )
 
 (def pagesfile "pages.txt")
 (def pagecount 10000)
-(def pagesrank (vec (repeat pagecount 1)))
+(def pageranks (vec (repeat pagecount 1)))
 (def weight 0.85)
 
 (defn Reader []
   (with-open [rdr (clojure.java.io/reader pagesfile)]
     (reduce conj [] (line-seq rdr))
   )  
+)
+
+(defn Contains [vector value]
+  (some #(= (str value) %) vector)
 )
 
 (defn Summation [expression count]
@@ -22,16 +27,46 @@
   )
 )
 
-(defn PageRank [page] 
-  (+ (- 1 weight) (* weight (Summation 1 10))) 
+(defn CalculatePageRank [expression count] 
+  (+ (- 1 weight) (* weight (Summation expression count))) 
+)
+
+(defn GetPageRank [index]
+  (get pageranks index)
+)
+
+(defn GetOutboundPages [pages from]
+  (get pages from)
+)
+(defn GetInboundPages [pages to]
+
+  (loop [from 0, inbound []]
+    (if (< from pagecount)
+      (if (Contains (get pages from) to)
+        (recur (+ from 1) (conj inbound from))
+        (recur (+ from 1) inbound)
+      )
+      inbound
+    )
+  )
 )
 
 (defn -main []
-  (println (PageRank 1))
-  ; (def pages [])
-  ; (def index (get [1, 2, 3] 0))
-  ; (print index)
-  ; (doseq [line (Reader)]
-  ;   (println line)
+  (def pages [])
+
+  (doseq [line (Reader)]
+    (def pages 
+      (conj pages (str/split line #" "))
+    )
+  )
+  
+  (println (GetInboundPages pages 0))
+  ; (def inbound 
+  ;   (conj inbound 
+  ;     if(Find ())
+  ;   )
   ; )
+
+  ; (println inbound)
+  
 )
